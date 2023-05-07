6912fc13c540f42aff38fb47f600b25ba3b41459
Duje Marusic <duje.marusic@typqast.com>
2023-05-07 23:22:27 +0200

    DB changes, relation and fd create, closures , candidate keys, minimal dependencies, non trivial dependencies

--- db.js
+++ db.js
@@ -1,108 +1,121 @@
 const db = [
     {
+        relation: ['A', 'B', 'C', 'D', 'E'],
+        dependencies: [
+            {
+                lhs: ['A'],
+                rhs: ['B', 'C']
+            },
+            {
+                lhs: ['B'],
+                rhs: ['C']
+            },
+             {
+                 lhs: ['D'],
+                 rhs: ['E']
+             }
+        ]
+    },
+    {
         // primjer 1 nije u 3 nf zbog tranzitivne funk ABCD->E->F
-        example: {
-            R: "A B C D E F G H I J",
-            FO: [{
-                lhs: "A B C D",
-                rhs: "E F G H I J"
-            },{
-                lhs: "A B C",
-                rhs: "D"
-            },{
-                lhs: "C D",
-                rhs: "G H"
-            },{
-                lhs: "E",
-                rhs: "F"
-            },{
-                lhs: "G H",
-                rhs: "J"
-            }] 
-        }
+        relation: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+        dependencies: [{
+            lhs: ['A', 'B', 'C', 'D'],
+            rhs: ['E', 'F', 'G', 'H', 'I', 'J']
+        },{
+            lhs: ['A', 'B', 'C'],
+            rhs: ['D']
+        },{
+            lhs: ['C', 'D'],
+            rhs: ['G', 'H']
+        },{
+            lhs: ['E'],
+            rhs: ['F']
+        },{
+            lhs: ['G', 'H'],
+            rhs: ['J']
+        }] 
     },
     {
-        example: {
-            R: "A B C D E F G H I J",
-            FO: [{
-                lhs: "A",
-                rhs: "C"
+            relation: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+            dependencies: [{
+                lhs: ['A'],
+                rhs: ['C']
             },{
-                lhs: "B",
-                rhs: "D"
+                lhs: ['B'],
+                rhs: ['D']
             },{
-                lhs: "A",
-                rhs: "E"
+                lhs: ['A'],
+                rhs: ['E']
             },{
-                lhs: "A C",
-                rhs: "B"
+                lhs: ['A', 'C'],
+                rhs: ['B']
             },{
-                lhs: "D",
-                rhs: "A"
+                lhs: ['D'],
+                rhs: ['A']
             }] 
-        },
     },
-    {
-        example: {
-            R: "A B C D E F G H I J",
-            FO: [{
-                lhs: "A B",
-                rhs: "D C"
-            },{
-                lhs: "D",
-                rhs: "A"
-            },{
-                lhs: "C",
-                rhs: "I"
-            },{
-                lhs: "A C",
-                rhs: "H"
-            },{
-                lhs: "D",
-                rhs: "J"
-            }] 
-        } 
-    },
-    {
-        example: {
-            R: "A B C D E F G H I J",
-            FO: [{
-                lhs: "A",
-                rhs: "C"
-            },{
-                lhs: "B",
-                rhs: "D"
-            },{
-                lhs: "A",
-                rhs: "E"
-            },{
-                lhs: "A C",
-                rhs: "B"
-            },{
-                lhs: "D",
-                rhs: "A"
-            }] 
-        },
-    },
-    {
-        example: {
-            R: "A B C D E F G H I J",
-            FO: [{
-                lhs: "A",
-                rhs: "C"
-            },{
-                lhs: "B",
-                rhs: "D"
-            },{
-                lhs: "A",
-                rhs: "E"
-            },{
-                lhs: "A C",
-                rhs: "B"
-            },{
-                lhs: "D",
-                rhs: "A"
-            }] 
-        },
-    }
+    // {
+    //     example: {
+    //         R: "A B C D E F G H I J",
+    //         FO: [{
+    //             lhs: "A B",
+    //             rhs: "D C"
+    //         },{
+    //             lhs: "D",
+    //             rhs: "A"
+    //         },{
+    //             lhs: "C",
+    //             rhs: "I"
+    //         },{
+    //             lhs: "A C",
+    //             rhs: "H"
+    //         },{
+    //             lhs: "D",
+    //             rhs: "J"
+    //         }] 
+    //     } 
+    // },
+    // {
+    //     example: {
+    //         R: "A B C D E F G H I J",
+    //         FO: [{
+    //             lhs: "A",
+    //             rhs: "C"
+    //         },{
+    //             lhs: "B",
+    //             rhs: "D"
+    //         },{
+    //             lhs: "A",
+    //             rhs: "E"
+    //         },{
+    //             lhs: "A C",
+    //             rhs: "B"
+    //         },{
+    //             lhs: "D",
+    //             rhs: "A"
+    //         }] 
+    //     },
+    // },
+    // {
+    //     example: {
+    //         R: "A B C D E F G H I J",
+    //         FO: [{
+    //             lhs: "A",
+    //             rhs: "C"
+    //         },{
+    //             lhs: "B",
+    //             rhs: "D"
+    //         },{
+    //             lhs: "A",
+    //             rhs: "E"
+    //         },{
+    //             lhs: "A C",
+    //             rhs: "B"
+    //         },{
+    //             lhs: "D",
+    //             rhs: "A"
+    //         }] 
+    //     },
+    // }
 ];
--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 var selectedExample;
+let relationsClosures = [];
+let cancidateKeys = [];
+let primeAttributes = [];
+let nonPrimeAttributes = [];
+let minimalCandidateKey = [];
+let minimalComposedCandidateKey = [];
+let minimalDependencies = [];
+let minimalCoverSet = [];
+let derivedFds = [];
 
 window.addEventListener("load", init);
 
@@ -15,11 +24,17 @@ function init() {
 }
 
 function loadExample(selectedValue) {
-    document.getElementById('dependencies').innerHTML = "";
-    selectedExample = db[selectedValue].example;
-    document.getElementById('relation').value = selectedExample.R;
+    if (selectedValue === "-1") {
+        document.getElementById('dependencies').innerHTML = "";
+        document.getElementById('relation').value = "";
+        return;
+    }
 
-    selectedExample.FO.forEach((d, i) => {
+    document.getElementById('dependencies').innerHTML = "";
+    selectedExample = db[selectedValue];
+    document.getElementById('relation').value = selectedExample.relation;
+
+    selectedExample.dependencies.forEach((d, i) => {
         createEmptyDependancyInput(i);
         document.getElementById(`lhs-${i}`).value = d.lhs;
 		document.getElementById(`rhs-${i}`).value = d.rhs;
@@ -28,8 +43,7 @@ function loadExample(selectedValue) {
     document.getElementById('key-button').disabled = false;
 }
 
-function createEmptyDependancyInput(i)
-{
+function createEmptyDependancyInput(i) {
     let container = document.createElement('div');
     container.classList.add('flex-row')
 	container.innerHTML = `
@@ -40,36 +54,152 @@ function createEmptyDependancyInput(i)
     document.getElementById('dependencies').appendChild(container);
 }
 
-function findCandidateKeys() {
-    let allAttributes = selectedExample.R.split(' ');
-    let rightSide = selectedExample.FO.map(d => d.rhs.split(' '));
-    let leftSide = selectedExample.FO.map(d => d.lhs.split(' '));
-    let potentionalKeys = [];
-    let found = false;
+function addEmptyDependency() {
+    const leftSide = Array.from(document.querySelectorAll('[id^="lhs-"]'));
+    const rightSide = Array.from(document.querySelectorAll('[id^="rhs-"]'));
 
-    allAttributes.forEach(attr => {
-        found = false;
-        rightSide.forEach(r => {
-            if (r.find(e => e === attr))
-                found = true;
-        })
-        if (!found) {
-            potentionalKeys.push(attr)
+    if (!leftSide.length || !rightSide.length) {
+        return createEmptyDependancyInput(0);
+    }
+
+    if (leftSide[leftSide.length - 1].value === '' || rightSide[rightSide.length - 1].value === '') {
+        return;
+    }
+
+    createEmptyDependancyInput(leftSide.length);
+}
+
+function getAllSubsets(arr) {
+    const subsets = [[]];
+    
+    for (const el of arr) {
+        const last = subsets.length-1;
+        for (let i = 0; i <= last; i++) {
+            subsets.push( [...subsets[i], el] );
+        }
+    }
+    return subsets.slice(1);
+}
+
+function findClosure(attribute, leftSide, rightSide) {
+    let startingClosure = [...attribute];
+    let currentClosure = attribute;
+
+    for (let i = 0; i < leftSide.length; i++) {
+        const lhs = leftSide[i];
+        if (lhs.every(val => startingClosure.includes(val))) {
+            if (rightSide[i]) {
+                currentClosure = Array.from(new Set([...currentClosure, ...rightSide[i] ]));
+            }
+        }
+    }
+    return currentClosure;
+}
+
+function findPrimaryKeys() {
+    let allAttributes = document.getElementById('relation').value.split(',');
+    let leftSide = Array.from(document.querySelectorAll('[id^="lhs-"]')).map(element => [...element.value.split(',')]);
+    let rightSide = Array.from(document.querySelectorAll('[id^="rhs-"]')).map(element => [...element.value.split(',')]);
+    let allSubsets = getAllSubsets(allAttributes);
+
+    allSubsets.forEach(attr => {
+        const closure = findClosure(attr, leftSide, rightSide);
+        
+        if (allAttributes.every(c => closure.includes(c))) {
+
+            if (attr.length === 1) {
+                minimalComposedCandidateKey = [];
+                minimalCandidateKey = [...attr];
+            }
+
+            if (!minimalCandidateKey.length && (minimalComposedCandidateKey.length > attr.length || !minimalComposedCandidateKey.length)) {
+                minimalComposedCandidateKey = [...attr]
+            }
+
+            cancidateKeys.push(attr);
+        }
+        relationsClosures.push({ lhs: attr, rhs: closure });
+    });
+
+    console.log("Relation closures -> ", relationsClosures);
+    console.log("CANDIDATE KEYS: -> ", cancidateKeys);
+    console.log("Minimal candidate key -> ", minimalCandidateKey)
+    console.log("Minimal composed candidate key -> ", minimalComposedCandidateKey)
+
+    primeAttributes = minimalCandidateKey.length ? minimalCandidateKey : minimalComposedCandidateKey;
+    nonPrimeAttributes = allAttributes.filter(attr => !primeAttributes.includes(attr));
+
+    console.log("Prime attributes -> ", primeAttributes);
+    console.log("Non-Prime attributes -> ", nonPrimeAttributes);
+
+    minimalCoverSet = findMinimalCoverSet(leftSide, rightSide);
+    minimalDependencies = findMinimalDependencies(minimalCoverSet);
+    derivedFds = calculateDerivedFds(relationsClosures);
+}
+
+function findMinimalCoverSet(leftSide, rightSide) {
+    let fdMin = [];
+
+    for (let i = 0; i < rightSide.length; i++) {
+        const rhs = rightSide[i];
+
+        if (rhs.length > 1) {
+            rhs.forEach(e => {
+                fdMin.push({ lhs: leftSide[i], rhs: [e]});
+            });
+            continue;
+        }
+
+        fdMin.push({ lhs: leftSide[i], rhs: rhs});
+    }
+    
+    return fdMin;
+}
+
+function findMinimalDependencies(minCover) {
+    const minimalDependencies = [];
+    const minLhs = minCover.map(md => md.lhs);
+    const minRhs = minCover.map(md => md.rhs);
+
+    minCover.forEach((minD, i) => {
+        const tempRhs = [...minRhs];
+        tempRhs.splice(i, 1);
+        const closure = findClosure(minD.lhs, minLhs, tempRhs)
+        
+        if (!closure.includes(minRhs[i][0])) {
+            console.log("Necessary fd -> ", minD);
+            minimalDependencies.push(minD);
         }
     });
 
-    displayCandidateKeys(potentionalKeys);
+    console.log("Minimal fds -> ", minimalDependencies);
+    return minimalDependencies;
 }
 
-function minimize(attributes) {
+function calculateDerivedFds(closures) {
+    const derivedFds = [];
+    closures.forEach(c => {
+        const rhs = [];
+        c.rhs.forEach(attr => {
+            if (!c.lhs.includes(attr)) {
+                rhs.push(attr);
+            }
+        });
+
+        if (rhs.length) {
+            derivedFds.push({ lhs: c.lhs, rhs: rhs});
+        }
+    })
+    console.log("Set of non trivial fds -> ", derivedFds);
+    return derivedFds;
+}
+
+function calculateNormalForms() {
 
 }
 
-function displayCandidateKeys(keys) {
-    let result = document.getElementById('result');
-    let container = document.createElement(`span`);
-    container.innerHTML = keys.join();
-
-    result.innerHTML = '';
-    result.appendChild(container);
+function calcSecondNf() { 
+    if (minimalCandidateKey.length) {
+        console.log("Relation is in 2NF because there are no composite minimum keys");
+    }
 }
